3ee2c6050e7961db65e8544f3df87a5517ff313b
fwcd <fwcd@fwcd.dev>
2024-04-15 01:21:43 +0200

    Factor out LinkAdapter

---
+++ src/adapter.rs
@@ -0,0 +1,58 @@
+use anyhow::{Context, Result};
+use midly::{live::LiveEvent, MidiMessage};
+use rusty_link::{AblLink, SessionState};
+use tracing::{info, trace};
+
+pub struct LinkAdapter {
+    link: AblLink,
+    state: SessionState,
+}
+
+impl LinkAdapter {
+    /// Creates a new adapter that automatically connects to Ableton Link.
+    pub fn new() -> Self {
+        let link = AblLink::new(120.0);
+        let state = SessionState::new();
+
+        info!("Enabling Ableton Link");
+        link.enable(true);
+
+        Self { link, state }
+    }
+
+    /// Handles an incoming MIDI event in raw form.
+    pub fn handle_raw_event(&mut self, raw: &[u8]) -> Result<()> {
+        let event = LiveEvent::parse(raw).context("Could not parse MIDI event")?;
+        self.handle_event(event)
+    }
+
+    /// Handles an incoming MIDI event.
+    pub fn handle_event(&mut self, event: LiveEvent) -> Result<()> {
+        match event {
+            LiveEvent::Midi { channel: _, message: MidiMessage::NoteOn { key, vel } } => {
+                // See https://github.com/mixxxdj/mixxx/wiki/MIDI%20clock%20output
+                match key.as_int() {
+                    // BPM
+                    52 => {
+                        let bpm = (vel.as_int() + 50) as f64;
+                        info!(bpm, "Setting BPM");
+                        self.link.capture_audio_session_state(&mut self.state);
+                        self.state.set_tempo(bpm, 0);
+                        self.link.commit_audio_session_state(&self.state);
+                    },
+                    _ => trace!(?event, "Ignoring MIDI note event"),
+                }
+            },
+            _ => trace!(?event, "Ignoring MIDI event"),
+        }
+
+        Ok(())
+    }
+}
+
+impl Drop for LinkAdapter {
+    fn drop(&mut self) {
+        info!("Disabling Ableton Link");
+        self.link.enable(false);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 #[cfg(not(unix))]
 compile_error!("MIDILink currently requires a Unix-like platform (e.g. macOS or Linux) since virtual MIDI ports are not supported on Windows");
 
+mod adapter;
+
 use std::thread;
 
 use anyhow::{anyhow, Result};
 use clap::Parser;
 use midir::{os::unix::{VirtualInput, VirtualOutput}, MidiInput, MidiOutput};
-use midly::{live::LiveEvent, MidiMessage};
-use rusty_link::{AblLink, SessionState};
-use tracing::{info, trace, warn};
+use tracing::{info, warn};
+
+use crate::adapter::LinkAdapter;
 
 #[derive(Parser)]
 #[command(version)]
@@ -23,38 +25,16 @@ fn main() -> Result<()> {
     _ = dotenvy::dotenv();
 
     let args = Args::parse();
-    let link = AblLink::new(120.0);
-    let mut state = SessionState::new();
-
-    info!("Enabling Ableton Link");
-    link.enable(true);
-    // TODO: Use Drop to .enable(false) once the app is quit
+    let mut adapter = LinkAdapter::new();
 
     info!(name = %args.name, "Creating virtual MIDI ports");
     let midi_in = MidiInput::new("MIDILink input")?;
     let _conn_in = midi_in.create_virtual(
         &args.name,
         move |_stamp, raw, _| {
-            // TODO: Factor this whole callback into a Result-returning function
-            match LiveEvent::parse(raw) {
-                Ok(event) => match event {
-                    LiveEvent::Midi { channel: _, message: MidiMessage::NoteOn { key, vel } } => {
-                        // See https://github.com/mixxxdj/mixxx/wiki/MIDI%20clock%20output
-                        match key.as_int() {
-                            // BPM
-                            52 => {
-                                let bpm = (vel.as_int() + 50) as f64;
-                                info!(bpm, "Setting BPM");
-                                link.capture_audio_session_state(&mut state);
-                                state.set_tempo(bpm, 0);
-                                link.commit_audio_session_state(&state);
-                            },
-                            _ => trace!(?event, "Ignoring MIDI note event"),
-                        }
-                    },
-                    _ => trace!(?event, "Ignoring MIDI event"),
-                },
-                Err(err) => warn!(?err, "Could not parse MIDI event"),
+            let result = adapter.handle_raw_event(raw);
+            if let Err(e) = result {
+                warn!(%e, "Error while handling event");
             }
         },
         ()
