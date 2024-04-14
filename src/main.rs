6c77dd73d39972ac254194ef977fcee6aaaba2d1
fwcd <fwcd@fwcd.dev>
2024-04-15 00:10:06 +0200

    Set up basic MIDI connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+#[cfg(not(unix))]
+compile_error!("MIDILink currently requires a Unix-like platform (e.g. macOS or Linux) since virtual MIDI ports are not supported on Windows");
+
+use std::thread;
+
+use anyhow::{anyhow, Result};
+use clap::Parser;
+use midir::{os::unix::{VirtualInput, VirtualOutput}, MidiInput, MidiOutput};
+use tracing::info;
+
+#[derive(Parser)]
+#[command(version)]
+struct Args {
+    /// The name of the virtual MIDI input and output ports.
+    #[arg(short, long, default_value = "Link")]
+    name: String,
+}
+
+fn main() -> Result<()> {
+    tracing_subscriber::fmt().init();
+    _ = dotenvy::dotenv();
+
+    let args = Args::parse();
+
+    info!(name = %args.name, "Creating virtual MIDI ports");
+
+    let midi_in = MidiInput::new("MIDILink input")?;
+    let _conn_in = midi_in.create_virtual(
+        &args.name,
+        move |stamp, message, _| {
+            info!(stamp, ?message, "Received");
+        },
+        ()
+    ).map_err(|e| anyhow!("Could not create virtual input: {}", e))?;
+
+    let midi_out = MidiOutput::new("MIDILink output")?;
+    let _conn_out = midi_out.create_virtual(&args.name)
+        .map_err(|e| anyhow!("Could not create virtual input: {}", e))?;
+
+    info!("Waiting for input...");
+
+    loop {
+        thread::park();
+    }
 }
